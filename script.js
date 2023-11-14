c5f670f7a54e1edd80854624456ed69e6676f4fe
Raju Kumar <129931026+Rajusudha@users.noreply.github.com>
2023-11-14 09:19:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-<script>
+       <script>
 function myFunction() {
-  alert("Hello! I am an alert box!");
+  alert("First Name:virat");
+}
+function myFunction1() {
+  alert("Last Name:kohil");
+}
+function myFunction2() {
+  alert("Phone Number:7870356995");
+}
+function myFunction3() {
+  alert("Email ID:rajukumar20182018@gmail.com");
 }
 </script>
